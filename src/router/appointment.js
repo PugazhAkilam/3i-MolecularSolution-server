3dd7af816c8799da4864d469f8051cfbeb5f163e
nithyasri-akilam <nithyasri@akilamtechnology.com>
2025-10-04 18:13:26 +0530

    visitPatientId

--- src/router/appointment.js
+++ src/router/appointment.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const { poolPromise, sql } = require('../config/db'); // Adjust path as needed
-const { createAppointment, getTodayAppointments, removeAppointment, updateAppointment, getAppointmentsData, getVisitDetails} = require('../controller/appointmentController');
+const { createAppointment, getTodayAppointments, removeAppointment, updateAppointment, getAppointmentsData, getVisitDetails, getVisitDetails2} = require('../controller/appointmentController');
 
 router.post('/createAppointment', createAppointment);
 
@@ -16,6 +16,8 @@ router.put('/editAppointment/:id', updateAppointment);
 
 // GET /api/appointment/withPatientDetails
 router.get('/visitDetails', getVisitDetails);
+
+router.get('/visitDetails2/:id', getVisitDetails2);
 
 
 router.get('/appointmentDetails/:id', getAppointmentsData);
